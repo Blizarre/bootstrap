00bcd0626ac9df8d2eda97130e498f8621c534b9
simon <simon@genestack.com>
2016-05-11 12:16:20 +0100

    Fix for sudoless ubuntus

--- Readme.md
+++ Readme.md
@@ -5,5 +5,5 @@ NOTE: mainly for ubuntu systems
 Copy-paste this in a terminal:
 
 ```
-apt-get update && apt-get -y install curl && curl https://raw.githubusercontent.com/Blizarre/bootstrap/master/bootstrap.sh | /bin/sh
+if [ "$EUID" -ne 0 ]; then SUDO="sudo"; fi; $SUDO apt-get update && $SUDO apt-get -y install curl && curl https://raw.githubusercontent.com/Blizarre/bootstrap/master/bootstrap.sh | /bin/sh
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,20 @@
 #! /bin/sh
+
 set -e
 
-export BOOTSTRAP_DIR=~/git/bootstrap
-export GIT_REPO=https://github.com/Blizarre/bootstrap.git
+BOOTSTRAP_DIR=~/git/bootstrap
+GIT_REPO=https://github.com/Blizarre/bootstrap.git
 
-sudo apt-get -y update 
-sudo apt-get -y upgrade
-sudo apt-get -y install curl vim git tmux
+# Do not use sudo if the script is run as root, as sudo might not be available
+SUDO=""
+if [ "$EUID" -ne 0 ]; then
+    SUDO=sudo
+fi
+
+$SUDO apt-get -y update
+$SUDO apt-get -y upgrade
+$SUDO apt-get -y install curl vim git tmux
+
 mkdir -p $BOOTSTRAP_DIR
 git clone $GIT_REPO $BOOTSTRAP_DIR
 ln -s $BOOTSTRAP_DIR/vimrc ~/.vimrc 
