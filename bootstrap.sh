81bf36706351bc7c4be802813f802ae5aff119de
simon <simon@genestack.com>
2016-05-11 13:31:59 +0100

    added python pip and syntax checker

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,14 +13,20 @@ fi
 
 $SUDO apt-get -y update
 $SUDO apt-get -y upgrade
-$SUDO apt-get -y install shellcheck curl vim git tmux
+$SUDO apt-get -y install python2.7 shellcheck curl vim git tmux
 
 mkdir -p $BOOTSTRAP_DIR
 git clone $GIT_REPO $BOOTSTRAP_DIR
-ln -s $BOOTSTRAP_DIR/vimrc ~/.vimrc 
+ln -s $BOOTSTRAP_DIR/vimrc ~/.vimrc
 ln -s $BOOTSTRAP_DIR/tmux.config ~/.tmux.conf
 
-# Vim plugins and syntax checker
+# Python pip and syntax checker
+curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
+$SUDO python2.7 get-pip.py
+rm get-pip.py
+$SUDO pip2.7 install flake8
+
+# Vim plugins + syntax checker
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
